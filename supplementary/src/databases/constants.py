b012ec6daa2bb94c091f0948cdba61f850f66b3c
myxp-lyp <81818403+myxp-lyp@users.noreply.github.com>
2026-08-04 02:02:28 +0100

    Delete supplementary directory

--- supplementary/src/databases/constants.py
+++
@@ -1,5 +0,0 @@
-labels = ["A", "D", "F", "H", "N", "S"]
-label2id = {label: i for i, label in enumerate(labels)}
-id2label = {i: label for i, label in enumerate(labels)}
-
-demographic_columns = ["AgeGroup", "Ethnicity", "Sex", "Race"]
--- supplementary/src/fairness/constants.py
+++
@@ -1,7 +0,0 @@
-# Define binary mappings for each demographic feature
-privileged_groups = {
-    "Sex": ["Male"],
-    "Race": ["Caucasian"],
-    "AgeGroup": ["20-25", "25-35"],
-    "Ethnicity": ["Not Hispanic"],
-}
